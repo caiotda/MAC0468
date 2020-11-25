e0a57424072fe8df0615febb48c01dac5636df4c
Caio Andrade <caiotdandrade@gmail.com>
2020-11-25 13:34:11 -0300

    [EP09] Aplica detecção de cantos de Shi-Tomasi

--- EPs/EP09/cantos.py
+++ EPs/EP09/cantos.py
@@ -78,7 +78,14 @@ def main():
     img = captura_imagem()
     if img is None:
         print("Erro na abertura da imagem.")
-    cv.imshow("Entrada", img)
+
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    corners = cv.goodFeaturesToTrack(gray,100,0.01,7)
+    corners = np.int0(corners)
+    for i in corners:
+        x,y = i.ravel()
+        cv.circle(img,(x,y),3,255,-1)
+    cv.imshow("Selecione Cantos", img)
     cv.waitKey(0)
     
 if __name__== '__main__':
